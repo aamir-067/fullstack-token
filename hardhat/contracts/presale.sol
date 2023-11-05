8d391bb954e3e0676b464d36efc25c77a50c1bda
aamir khan <aamirkhan@engineer.com>
2023-11-05 01:35:40 -0700

    completed contract unit tests

--- hardhat/contracts/presale.sol
+++ hardhat/contracts/presale.sol
@@ -1,22 +1,8 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
-// import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-interface IERC20CUSTOM{
-    /**
-     * @dev Addeed by the developer.
-     * This function returns the decimals of the token.
-     */
-    function decimals() external view returns (uint8);
-
-
-     /**
-     * @dev Addeed by the developer.
-     * This function mint new tokens.
-     */
-    function mint(address to,uint amount) external;
-}
-
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "./IERC20CUSTOM.sol";
 contract PreSale{
     // for fetching the eth price in usd.
     AggregatorV3Interface internal dataFeed;
@@ -38,12 +24,14 @@ contract PreSale{
     
     function purchaseTokens(uint tokens) public  payable{
         require(msg.value >= ((tokens / 1000) * currentRate), "not enough money to purchase tokens");  // 10 ** IERC20(mytoken).decimals() == 1000
-        require((totalSoldAmount + (tokens / 1000) <= (80000000 * 1000)), "token already sold");    // 1000 == IERC20(mytoken).decimals()
+        require((totalSoldAmount + tokens <= (80000000 * 1000)), "token already sold");    // 1000 == IERC20(mytoken).decimals()
 
-        if(!(totalSoldAmount + tokens >= (10000000 * 1000))){  // sold more then 10M token which means change price to mormal.
+        
+        totalSoldAmount += tokens;
+        if(((totalSoldAmount/1000) > 1000000) && currentRate != 0.00005 ether){  // sold more then 10M token which means change price to mormal.
             currentRate = 0.00005 ether;
         }
-        totalSoldAmount += tokens;
+
         IERC20CUSTOM(mytoken).mint(msg.sender,tokens);
 
         emit tokenSold(msg.sender, tokens/1000);
